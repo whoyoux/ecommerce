a6ba78a50746c78b3c3cdf8ac572f5bbbe1ad84e
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-02-07 22:17:26 +0100

    added seed for db

---
+++ prisma/seed.js
@@ -0,0 +1,63 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const CATEGORIES = [
+	{
+		name: "Clothes",
+		slug: "/clothes",
+		image: "https://i.imgur.com/QkIa5tT.jpeg",
+	},
+	{
+		name: "Electronics",
+		slug: "/electronics",
+		image: "https://i.imgur.com/ZANVnHE.jpeg",
+	},
+	{
+		name: "Furniture",
+		slug: "/furniture",
+		image: "https://i.imgur.com/Qphac99.jpeg",
+	},
+	{
+		name: "Shoes",
+		slug: "/shoes",
+		image: "https://i.imgur.com/qNOjJje.jpeg",
+	},
+	{
+		name: "Miscellaneous",
+		slug: "/miscellaneous",
+		image: "https://i.imgur.com/BG8J0Fj.jpg",
+	},
+];
+
+const productsJson = require("../data/products.json");
+
+async function main() {
+	const categories = await prisma.$transaction(
+		CATEGORIES.map((category) => prisma.category.create({ data: category })),
+	);
+
+	const newData = productsJson.map((product) => ({
+		label: product.label,
+		description: product.description,
+		price: product.price,
+		images: product.images,
+
+		categoryId:
+			categories.find((category) => category.name === product.category.name)
+				?.id ?? "NULL",
+	}));
+
+	const products = await prisma.product.createMany({
+		data: newData,
+	});
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import DealsOfTheDay from "@/components/home-page/deals-of-the-day";
 import Featured from "@/components/home-page/featured";
 import Hero from "@/components/home-page/hero";
 
-export default function Home() {
+export default async function Home() {
 	return (
 		<main className="flex flex-col gap-8 pt-10 mx-auto max-w-screen-lg">
 			<Hero />

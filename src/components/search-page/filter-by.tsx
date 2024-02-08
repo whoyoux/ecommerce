b037cce961c0e1182223562148556a624e0e262c
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-02-08 14:43:55 +0100

    selected options now is visible on buttons

--- src/components/search-page/filter-by.tsx
+++ src/components/search-page/filter-by.tsx
@@ -1,8 +1,31 @@
 import { SeachParams } from "@/app/search/page";
 import { buttonVariants } from "@/components/ui/button";
 import { CATEGORIES } from "@/config/site-config";
-import { GetURLWithStates } from "@/lib/utils";
+import { GetURLWithStates, cn } from "@/lib/utils";
 import Link from "next/link";
+
+const PRICE_RANGES = [
+	{
+		min: "",
+		max: "",
+		label: "All",
+	},
+	{
+		min: "0",
+		max: "50",
+		label: "$0 - $50",
+	},
+	{
+		min: "50",
+		max: "100",
+		label: "$50 - $100",
+	},
+	{
+		min: "200",
+		max: "",
+		label: "Over $200",
+	},
+];
 
 const FilterBy = ({ searchParams }: { searchParams: SeachParams }) => {
 	const query = (searchParams.query || "") as string;
@@ -32,46 +55,74 @@ const FilterBy = ({ searchParams }: { searchParams: SeachParams }) => {
 		});
 	};
 
+	const isPriceSelected = (
+		_minPrice: string,
+		_maxPrice: string,
+		_label: string,
+	): boolean => {
+		// if (minPrice === _minPrice && maxPrice === _maxPrice) return true;
+		let flag = false;
+		if (minPrice !== "" && minPrice === _minPrice) {
+			flag = true;
+		}
+
+		if (maxPrice !== "" && maxPrice === _maxPrice) {
+			flag = true;
+		}
+
+		if (minPrice === "" && maxPrice === "" && _label === "All") {
+			flag = true;
+		}
+
+		return flag;
+	};
+
+	const isCategorySelected = (categoryLabel: string): boolean => {
+		if (categoryLabel === "All" && category === "") {
+			return true;
+		}
+
+		if (category.toLocaleLowerCase() === categoryLabel.toLocaleLowerCase())
+			return true;
+		return false;
+	};
+
 	return (
 		<div className="flex flex-col gap-4">
 			<h4 className="font-bold text-2xl">Filter by</h4>
 			<div className="flex flex-wrap gap-2">
-				<Link
-					href={setPriceRange("", "")}
-					className={buttonVariants({ variant: "outline" })}
-				>
-					All
-				</Link>
-				<Link
-					href={setPriceRange("0", "50")}
-					className={buttonVariants({ variant: "outline" })}
-				>
-					$0 - $50
-				</Link>
-				<Link
-					href={setPriceRange("50", "100")}
-					className={buttonVariants({ variant: "outline" })}
-				>
-					$50 - $100
-				</Link>
-				<Link
-					href={setPriceRange("200", "")}
-					className={buttonVariants({ variant: "outline" })}
-				>
-					Over $200
-				</Link>
+				{PRICE_RANGES.map((priceRange) => (
+					<Link
+						href={setPriceRange(priceRange.min, priceRange.max)}
+						className={cn(
+							buttonVariants({ variant: "outline" }),
+							isPriceSelected(priceRange.min, priceRange.max, priceRange.label)
+								? "border-primary"
+								: "",
+						)}
+						key={`filter-price-${priceRange.label}`}
+					>
+						{priceRange.label}
+					</Link>
+				))}
 			</div>
 			<div className="flex flex-wrap gap-2">
 				<Link
 					href={setCategory("")}
-					className={buttonVariants({ variant: "outline" })}
+					className={cn(
+						buttonVariants({ variant: "outline" }),
+						isCategorySelected("All") && "border-primary",
+					)}
 				>
 					All
 				</Link>
 				{CATEGORIES.map((category) => (
 					<Link
 						href={setCategory(category.name)}
-						className={buttonVariants({ variant: "outline" })}
+						className={cn(
+							buttonVariants({ variant: "outline" }),
+							isCategorySelected(category.name) && "border-primary",
+						)}
 						key={`filter-by-${category.name}`}
 					>
 						{category.name}
--- src/components/search-page/ratings.tsx
+++ src/components/search-page/ratings.tsx
@@ -3,11 +3,37 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { GetURLWithStates, cn } from "@/lib/utils";
 import Link from "next/link";
 
+const RATING_LEVELS = [
+	{
+		level: 4,
+		label: "4 stars & up",
+	},
+	{
+		level: 3,
+		label: "3 stars & up",
+	},
+	{
+		level: 2,
+		label: "2 stars & up",
+	},
+	{
+		level: 1,
+		label: "1 stars & up",
+	},
+];
+
 const Ratings = ({ searchParams }: { searchParams: SeachParams }) => {
 	const query = (searchParams.query || "") as string;
+	const rating = (searchParams.rating || "") as string;
 	const category = (searchParams.category || "") as string;
 	const minPrice = (searchParams.min_price || "") as string;
 	const maxPrice = (searchParams.max_price || "") as string;
+
+	const isRatingSelected = (ratingLevel: string): boolean => {
+		if (rating === ratingLevel) return true;
+
+		return false;
+	};
 
 	const setRating = (rating: string) => {
 		return GetURLWithStates({
@@ -22,30 +48,18 @@ const Ratings = ({ searchParams }: { searchParams: SeachParams }) => {
 	return (
 		<div className="flex flex-col gap-4">
 			<h4 className="font-bold text-2xl">Rating</h4>
-			<Link
-				href={setRating("4")}
-				className={cn(buttonVariants({ variant: "outline" }))}
-			>
-				4 stars & up
-			</Link>
-			<Link
-				href={setRating("3")}
-				className={cn(buttonVariants({ variant: "outline" }))}
-			>
-				3 stars & up
-			</Link>
-			<Link
-				href={setRating("2")}
-				className={cn(buttonVariants({ variant: "outline" }))}
-			>
-				2 stars & up
-			</Link>
-			<Link
-				href={setRating("1")}
-				className={cn(buttonVariants({ variant: "outline" }))}
-			>
-				1 stars & up
-			</Link>
+			{RATING_LEVELS.map((ratingLevel) => (
+				<Link
+					href={setRating(ratingLevel.level.toString())}
+					className={cn(
+						buttonVariants({ variant: "outline" }),
+						isRatingSelected(ratingLevel.level.toString()) && "border-primary",
+					)}
+					key={`filter-rating-${ratingLevel.label}`}
+				>
+					{ratingLevel.label}
+				</Link>
+			))}
 		</div>
 	);
 };

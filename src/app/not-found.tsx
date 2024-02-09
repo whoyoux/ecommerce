bab5e573748b668310ddee553622b0a492b29da4
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-02-09 15:27:26 +0100

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+	return (
+		<div className="w-full text-center mt-20">
+			<h1 className="text-4xl font-bold">Not found.</h1>
+		</div>
+	);
+};
+
+export default NotFound;

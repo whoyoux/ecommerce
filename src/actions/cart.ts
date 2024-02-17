c67f3e4f5c36144636d810c9688646e61a7d3117
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-02-17 20:59:46 +0100

    user can have max 10 qty of products

--- src/actions/cart.ts
+++ src/actions/cart.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { MAX_QUANTITY_PER_PRODUCTS } from "@/config/site-config";
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { addProductToCartSchema } from "@/validators/cartSchema";
@@ -77,8 +78,17 @@ export const addToCart = async (formData: FormData): Promise<Response> => {
 		const foundProductInCart = cart?.products?.find(
 			(p) => p.productId === productId,
 		);
+
 		if (foundProductInCart) {
 			// Update the quantity
+			if (foundProductInCart.quantity + quantity > MAX_QUANTITY_PER_PRODUCTS) {
+				return {
+					success: false,
+					code: "INVALID_DATA",
+					message: `You can't add more than ${MAX_QUANTITY_PER_PRODUCTS} of the same product to the cart.`,
+				};
+			}
+
 			await prisma.cart.update({
 				where: {
 					userId: session.user.id,
@@ -186,6 +196,8 @@ export const removeFromCart = async (productId: string): Promise<Response> => {
 	}
 };
 
+//TODO: Function must block the user from adding more than MAX_QUANTITY_PER_PRODUCTS products to the cart
+
 export const incrementQtyInCart = async (
 	productId: string,
 ): Promise<Response> => {
@@ -198,25 +210,89 @@ export const incrementQtyInCart = async (
 		};
 	}
 
+	// try {
+	// 	await prisma.cart.update({
+	// 		where: {
+	// 			userId: session.user.id,
+	// 		},
+	// 		data: {
+	// 			products: {
+	// 				update: {
+	// 					where: {
+	// 						id: productId,
+	// 					},
+	// 					data: {
+	// 						quantity: {
+	// 							increment: 1,
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	});
+
+	// 	revalidatePath("/cart");
+
+	// 	return {
+	// 		success: true,
+	// 		code: "PRODUCT_QUANTITY_UPDATED",
+	// 		message: "Product quantity updated",
+	// 	};
+	// } catch (err) {
+	// 	console.error(`Error incrementing product quantity in cart: ${err}`);
+	// 	return {
+	// 		success: false,
+	// 		code: "SERVER_ERROR",
+	// 		message: "Server error. Please try again later.",
+	// 	};
+	// }
+
 	try {
-		await prisma.cart.update({
-			where: {
-				userId: session.user.id,
-			},
-			data: {
-				products: {
-					update: {
-						where: {
-							id: productId,
-						},
-						data: {
-							quantity: {
-								increment: 1,
+		await prisma.$transaction(async (tx) => {
+			const cart = await tx.cart.findFirst({
+				where: {
+					userId: session.user.id,
+				},
+				include: {
+					products: true,
+				},
+			});
+
+			if (!cart || !cart.products) {
+				throw new Error("Cart not found or empty");
+			}
+
+			const product = cart.products.find((p) => p.id === productId);
+
+			if (!product) {
+				throw new Error("Product not found in cart");
+			}
+
+			if (product.quantity + 1 <= MAX_QUANTITY_PER_PRODUCTS) {
+				await tx.cart.update({
+					where: {
+						userId: session.user.id,
+					},
+					data: {
+						products: {
+							update: {
+								where: {
+									id: productId,
+								},
+								data: {
+									quantity: {
+										increment: 1,
+									},
+								},
 							},
 						},
 					},
-				},
-			},
+				});
+			} else {
+				throw new Error(
+					`You can't add more than ${MAX_QUANTITY_PER_PRODUCTS} of the same product to the cart.`,
+				);
+			}
 		});
 
 		revalidatePath("/cart");
@@ -227,7 +303,7 @@ export const incrementQtyInCart = async (
 			message: "Product quantity updated",
 		};
 	} catch (err) {
-		console.error(`Error incrementing product quantity in cart: ${err}`);
+		console.error(`Error decrementing product quantity in cart: ${err}`);
 		return {
 			success: false,
 			code: "SERVER_ERROR",
